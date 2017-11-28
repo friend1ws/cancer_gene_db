398587c2dba007f9a58e32a5dc7b2c812134dda5
friend1ws <friend1ws@gmail.com>
2017-11-28 17:09:41 +0900

    add process files for Vogelstein 2013 paper

---
+++ VogelsteinEtAl2013/proc_VogelsteinEtAl2013.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import xlrd, re
+
+hout = open("VogelsteinEtAl2013/VogelsteinEtAl2013.proc.txt", "w")
+
+book = xlrd.open_workbook("VogelsteinEtAl2013/1235122TablesS1-4.xlsx")
+
+main_sheet = book.sheet_by_index(5)
+
+gene2type = {}
+for row in range(2, main_sheet.nrows):
+    gene = main_sheet.cell(row, 0).value
+    classification = main_sheet.cell(row, 5).value
+    print >> hout, gene + '\t' + classification
+
+
+hout.close()
+
+
---
+++ prep_all.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+python CancerGeneCensus/proc_CancerGeneCensus.py
+
+python LawrenceEtAl2014/proc_LawrenceEtAl2014.py
+
+python YeEtAl2016/proc_YeEtAl2016.py 
+
+python VogelsteinEtAl2013/proc_VogelsteinEtAl2013.py
+
+
