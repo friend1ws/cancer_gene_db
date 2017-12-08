98925b755a6261a96908f3808620970b7937ce97
Yuichi Shiraishi <friend1ws@gmail.com>
2017-12-08 15:32:41 +0900

    modify VogelsteinEtAl2013.proc.txt

--- VogelsteinEtAl2013/proc_VogelsteinEtAl2013.py
+++ VogelsteinEtAl2013/proc_VogelsteinEtAl2013.py
@@ -11,6 +11,8 @@ main_sheet = book.sheet_by_index(5)
 gene2type = {}
 for row in range(2, main_sheet.nrows):
     gene = main_sheet.cell(row, 0).value
+    if gene.startswith("*"): continue
+
     classification = main_sheet.cell(row, 5).value
     print >> hout, gene + '\t' + classification
 
